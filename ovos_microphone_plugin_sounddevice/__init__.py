a6b9d3c636bc4d2b392954d84daf6eefc0d6572f
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-05-27 14:03:59 -0400

    Add blocksize and type to fixe audio input

--- ovos_microphone_plugin_sounddevice/__init__.py
+++ ovos_microphone_plugin_sounddevice/__init__.py
@@ -65,6 +65,8 @@ class SoundDeviceMicrophone(Microphone):
             samplerate=self.sample_rate,
             device=index,
             channels=1,
+            blocksize=2048,
+            dtype="int16"
             callback=self._stream_callback,
         )
         self.stream.start()
